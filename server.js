996463cd247408409495059ed6806bcc20a8a4d2
Yuktesh <yukteshc@gmail.com>
2020-07-04 12:53:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/pagecount', function (req, res) {
   }
   if (db) {
     db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
+      res.send('{ pageCounttoal: ' + count + '}');
     });
   } else {
     res.send('{ pageCount: -1 }');
